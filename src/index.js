996843d6707e07bd013f37637a98e74c58af32e8
vcolavin <vcolavin@gmail.com>
2019-04-06 10:40:00 -0700

    oops my commit history is a mess

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Map } from '@esri/react-arcgis';
 
+import './style.scss';
+import './reset.scss';
+
 ReactDOM.render(
-	<div style={{ height: '500px', width: '1000px' }}>
+	<div
+		className="esri-container"
+		// style={{ height: '500px', width: '1000px' }}
+	>
 		<Map />
 	</div>,
 	document.getElementById('root')
